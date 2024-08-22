5383dfa98a965180a42d6c48dd10a9ec1f8f9133
nexus <nebulawareconfigs@gmail.com>
2024-08-21 21:12:42 -0700

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -370,10 +370,10 @@ run(function()
 				if textChatService.ChatVersion == Enum.ChatVersion.TextChatService then
 					local oldchannel = textChatService.ChatInputBarConfiguration.TargetTextChannel
 					local newchannel = cloneref(game:GetService('RobloxReplicatedStorage')).ExperienceChat.WhisperChat:InvokeServer(v.UserId)
-					if newchannel then newchannel:SendAsync('helloimusinginhaler') end
+					if newchannel then newchannel:SendAsync('helloimusingnovoline') end
 					textChatService.ChatInputBarConfiguration.TargetTextChannel = oldchannel
 				elseif replicatedStorage:FindFirstChild('DefaultChatSystemChatEvents') then
-					replicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('/w '..v.Name..' helloimusinginhaler', 'All')
+					replicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('/w '..v.Name..' helloimusingnovoline', 'All')
 				end
 			end
 		end
@@ -381,8 +381,8 @@ run(function()
 
 	function whitelist:checkmessage(msg, plr)
 		local otherprio = self:get(plr)
-		if plr == lplr and msg == 'helloimusinginhaler' then return true end
-		if self.localprio > 0 and self.said[plr.Name] == nil and msg == 'helloimusinginhaler' and plr ~= lplr then
+		if plr == lplr and msg == 'helloimusingnovoline' then return true end
+		if self.localprio > 0 and self.said[plr.Name] == nil and msg == 'helloimusingnovoline' and plr ~= lplr then
 			self.said[plr.Name] = true
 			notif('Novoline', plr.Name..' is using Novoline!', 60)
 			self.customtags[plr.Name] = {{text = 'NOVOLINE USER', color = Color3.new(1, 1, 0)}}
@@ -480,7 +480,7 @@ run(function()
 			local bubblechat = exp:WaitForChild('bubbleChat', 5)
 			if bubblechat then
 				table.insert(vapeConnections, bubblechat.DescendantAdded:Connect(function(newbubble)
-					if newbubble:IsA('TextLabel') and newbubble.Text:find('helloimusinginhaler') then
+					if newbubble:IsA('TextLabel') and newbubble.Text:find('helloimusingnovoline') then
 						newbubble.Parent.Parent.Visible = false
 					end
 				end))
